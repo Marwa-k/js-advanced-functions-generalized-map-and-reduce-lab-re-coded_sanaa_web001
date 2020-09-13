2104605b6efe197e9e5c8e37e3c44f560d080864
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-13 15:54:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(arr,func){
+    const newArr = []
+    for (let i=0; i < arr.length; i++) {
+        newArr.push(func(arr[i]))
+    }
+    return newArr
+}
+
+function mapToNegativize(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(-1 * src[i]) // Unique work
+  }
+  return r
+}
